d6939da03f9bf289872984bba4c7e2a87b6af6db
Sami Hilal <sami.hilal@mail.mcgill.ca>
2022-01-16 14:18:37 -0500

    chnaged the git ignore to exclude .env

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 //https://www.mongodb.com/cloud/atlas
 
 
-//const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+//const PORT = 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
